4ec15a80b2a88e0887cae24436a49ea6a744a3b2
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-09-19 18:59:44 +0200

    Schedule modified

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,3 +4,9 @@ end
 every '15 14,23 * * 1-6' do
   command "cd /home/galiclick/public_html/rodaben/current && bundle exec rake RAILS_ENV=production products:to_rodaben_eurotyre"
 end
+every :day, :at => '2:00pm' do
+  command "backup perform --trigger rodaben_backup"
+end
+every :day, :at => '0:30am' do
+  command "backup perform --trigger rodaben_backup"
+end
