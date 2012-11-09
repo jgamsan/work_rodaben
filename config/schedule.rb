a0256862248634a0871d38747da939d720b90efb
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-11-09 13:22:58 +0100

    Mas mejoras

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,15 +1,15 @@
-every '45 02,14 * * 1-6' do
+every '45 02,14 * * 1-7', :roles => [:app] do
   command "cd /home/galiclick/public_html/rodaben/current && bundle exec rake RAILS_ENV=production products:to_rodaben_gane"
 end
-every '15 04,14 * * 1-6' do
+every '15 04,14 * * 1-7', :roles => [:app] do
   command "cd /home/galiclick/public_html/rodaben/current && bundle exec rake RAILS_ENV=production products:to_rodaben_eurotyre"
 end
-every :day, :at => '5:00am' do
+every :day, :at => '5:00am', :roles => [:app] do
   command "cd /home/galiclick/public_html/rodaben/current && bundle exec rake RAILS_ENV=production products:clean_rodaben"
 end
-every :day, :at => '2:00pm' do
+every :day, :at => '2:00pm', :roles => [:app] do
   command "backup perform --trigger rodaben_backup"
 end
-every :day, :at => '0:30am' do
+every :day, :at => '0:30am', :roles => [:app] do
   command "backup perform --trigger rodaben_backup"
 end
