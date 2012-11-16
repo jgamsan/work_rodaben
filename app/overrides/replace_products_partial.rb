cc22167e5b9e1dcff81cd6217c6a16f5649c12dd
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-11-17 00:29:43 +0100

    Mas mejoras

--- app/overrides/replace_products_partial.rb
+++ app/overrides/replace_products_partial.rb
@@ -2,19 +2,24 @@ Deface::Override.new(:virtual_path => %q{spree/shared/_products},
                           :name => %q{replace_products_partial},
                           :replace => %q{#products[data-hook]},
                           :text => %q{
-  <div id="products" class="row" style="margin-top:10px;"data-hook>
-  <% reset_cycle('default') %>
+  <div id="products" style="margin-bottom: 20px;"data-hook>
   <% products.each do |product| %>
     <% if Spree::Config[:show_zero_stock_products] || product.has_stock? %>
-        <div id="product_<%= product.id %>" class="<%= cycle("span2","span2","span2","span2", :name => "classes") %>"data-hook="products_list_item" itemscope itemtype="http://schema.org/Product">
-        <div class="product-image">
+      <div class="row">
+        <div class="span1">
           <%= link_to small_image(product, :itemprop => "image"), product, :itemprop => 'url' %>
         </div>
-        <%= link_to truncate(product.name, :length => 50), product, :class => 'info', :itemprop => "name", :title => product.name %>
-        <span class="price selling" itemprop="price"><%= product.display_price %></span>
+        <div class="span5">
+          <h4><%= link_to truncate(product.name, :length => 50), product, :class => 'info', :itemprop => "name", :title => product.name %></h4>
+          <div class="row">
+            <div class="span3"><h4><%= product.taxons.last.name %></h4></div>
+            <div class="span2"><span class="badge badge-info price"><%= product.display_price %></span></div>
+          </div>
+        </div>
+    <div class="span2">
+     <a class="btn btn-primary btn-cart" href="cart.html">Add to cart</a>
     </div>
+  </div>
     <% end %>
   <% end %>
-  <% reset_cycle("classes") %>
-</div>
-                          })
+</div>})
