a466d8a5cd979f315be68d73f0285834682b5371
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-11-29 00:18:09 +0100

    mas mejoras

--- app/overrides/replace_payment.rb
+++ app/overrides/replace_payment.rb
@@ -3,6 +3,9 @@ Deface::Override.new(:virtual_path => %q{spree/checkout/_payment},
                           :replace => %q{[data-hook="buttons"]},
                           :text => %q{
       <div class="form-buttons" data-hook="buttons">
-  <%= submit_tag t(:save_and_continue), :class => 'btn btn-primary' %>
+  <%#= submit_tag t(:save_and_continue), :class => 'btn btn-primary' %>
+  <button type="submit" class="btn btn-primary">
+            <i class="icon-thumbs-up icon-white"></i> <%= t(:save_and_continue) %>
+          </button>
   <script>disableSaveOnClick();</script>
 </div>})
